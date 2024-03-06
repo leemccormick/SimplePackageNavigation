f608f483a49727589eb749b2ea859ddc29ab6f21
Lee McCormick <lee.mccormick@dialexa.com>
2024-03-06 09:53:46 -0600

    Run Micro Apps using Main App
    - Added Configuration --> Debug Staging, Release Staging,  Debug Micro, Release Micro...
    - Updated Product bundle identifier and swift complier flag
    - Edit Schema for each micro app to able to execute using Main App and updated the configure to Micro, also set environment variable to each micro app
    - On Main App, Window, using environment flag to show each View from micro app if the flag is micro app

--- SimplePackageNavigation/SimplePackageNavigationApp.swift
+++ SimplePackageNavigation/SimplePackageNavigationApp.swift
@@ -1,11 +1,45 @@
 import SwiftUI
 import TabMicro
 
+// MARK: - Import Micro Apps
+#if MICRO_DEBUG
+import MicroApp1
+import MicroApp2
+import FeatureView
+#endif
+
+// MARK: - App
 @main
 struct SimplePackageNavigationApp: App {
+    
     var body: some Scene {
         WindowGroup {
+#if MICRO_DEBUG
+            if ProcessInfo.processInfo.environment["microApp1"] == "true" {
+                MicroApp1NavigationView()
+            } else if ProcessInfo.processInfo.environment["microApp2"] == "true" {
+                MicroApp2NavigationView()
+            } else if ProcessInfo.processInfo.environment["commonUI"] == "true" {
+                CommonView1()
+            } else {
+                EmptyView()
+            }
+#else
             BottomTabView()
+#endif
+            
         }
     }
 }
+
+// MARK: - Environment Example
+
+//#if MICRO_DEBUG
+//print("✅MICRO_DEBUG")
+//#elseif DEV_DEBUG
+//print("✅DEV_DEBUG")
+//#elseif STAG_DEBUG
+//print("✅STAG_DEBUG")
+//#elseif PROD_DEBUG
+//print("✅PROD_DEBUG")
+//#endif
