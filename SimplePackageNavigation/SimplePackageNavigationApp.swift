cc995cc2c1f1118681375a81986f4c02a840e89e
Lee McCormick <lee.mccormick@dialexa.com>
2024-03-14 13:38:00 -0500

    Added Xcode Run Target for MicroApp3, MicroApp4, MicroApp5 and Tab3MicroApp So it can run seperately.
    - On Each Package target, go to edit scheme, and add executable and change environment to Debug Micro or Release Micro
    - On Main App, added logic to run each app.

--- SimplePackageNavigation/SimplePackageNavigationApp.swift
+++ SimplePackageNavigation/SimplePackageNavigationApp.swift
@@ -6,6 +6,10 @@ import AppConfig
 #if MICRO_DEBUG
 import MicroApp1
 import MicroApp2
+import MicroApp3Navigation
+import MicroApp4Navigation
+import MicroApp5Navigation
+import Tab3Navigation
 import FeatureView
 #endif
 
@@ -34,14 +38,43 @@ struct ContentViewBasedOnConfiguration: View  {
             MicroApp1NavigationView()
         } else if ProcessInfo.processInfo.environment["microApp2"] == "true" {
             MicroApp2NavigationView()
+        } else if ProcessInfo.processInfo.environment["microapp3"] == "true" {
+            MicroApp3NavigationView()
+        } else if ProcessInfo.processInfo.environment["microapp4"] == "true" {
+            MicroApp4NavigationView()
+        } else if ProcessInfo.processInfo.environment["microapp5"] == "true" {
+            MicroApp5NavigationView()
+        } else if ProcessInfo.processInfo.environment["tab3Navigation"] == "true" {
+            Tab3NavigationView()
         } else if ProcessInfo.processInfo.environment["commonUI"] == "true" {
             CommonView1()
         } else {
-            EmptyView()
+            ErrorRunMicroAppView()
         }
 #else
         BottomTabView()
 #endif
+    }
+}
+
+// MARK: - ErrorRunAppView
+struct ErrorRunMicroAppView: View  {
+    var body: some View {
+        VStack {
+            HStack {
+                Text("❌ ERROR !")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .foregroundColor(.red.opacity(0.80))
+                Spacer()
+            }
+            
+            Text("Something went wrong. Unable to find micro app to run, please ! check enviroment variable.")
+                .font(.callout)
+        }
+        .padding()
+        .background(Color.red.opacity(0.20))
+        .padding()
     }
 }
 
